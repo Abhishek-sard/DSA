af2d341418363594eedf510ae1f12bb48f3c93a4
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-05-19 22:06:31 +0545

    Add files via upload

--- PostfixToinfix algorithm.c
+++ PostfixToinfix algorithm.c
@@ -1,71 +1,61 @@
-//scan the given postfix expression from left ot right.
-//if character is operand push it into the stack
-//if the char is an operator,pop the two values from stack.
-//concatenate operator with two values(2 nd top vlue + operator + 1st value)
-//push this resulting strinng back into the stack
-// repet processs until the end of postfix now the value is stack in infix expression
+// //scan the given postfix expression from left ot right.
+// //if character is operand push it into the stack
+// //if the char is an operator,pop the two values from stack.
+// //concatenate operator with two values(2 nd top vlue + operator + 1st value)
+// //push this resulting strinng back into the stack
+// // repet processs until the end of postfix now the value is stack in infix expression
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 
-#define MAX_SIZE 100
-
-// Function to push a string onto the stack
-void push(char stack[MAX_SIZE][MAX_SIZE], int *top, char *item) {
-    if (*top == MAX_SIZE - 1) {
-        printf("Stack Overflow\n");
-        return;
-    }
-    strcpy(stack[++(*top)], item);
-}
-
-// Function to pop a string from the stack
-void pop(char stack[MAX_SIZE][MAX_SIZE], int *top, char *result) {
-    if (*top == -1) {
+ #define MAX_SIZE 100
+char *pop(char *stack[], int *top)
+{
+    if (*top = -1)
+    {
         printf("Stack Underflow\n");
-        return;
+        return NULL;
     }
-    strcpy(result, stack[(*top)--]);
+    return stack[(*top)--];
 }
-
-// Function to check if a character is an operator
-int is_operator(char ch) {
+int is_operator(char ch)
+{
     return (ch == '+' || ch == '-' || ch == '*' || ch == '/');
 }
-
-// Function to convert postfix expression to infix expression
-void postfix_to_infix(char *postfix, char *infix) {
-    char stack[MAX_SIZE][MAX_SIZE];
+void postfix_to_infix(char *postfix, char *infix)
+{
+    char *stack[MAX_SIZE];
     int top = -1;
-    char op1[MAX_SIZE], op2[MAX_SIZE], temp[MAX_SIZE];
-
-    for (int i = 0; postfix[i] != '\0'; i++) {
-        if (isalnum(postfix[i])) {  // If the character is an operand
-            char operand[2] = {postfix[i], '\0'};
-            push(stack, &top, operand);
-        } else if (is_operator(postfix[i])) {  // If the character is an operator
-            pop(stack, &top, op2);
-            pop(stack, &top, op1);
-            snprintf(temp, MAX_SIZE, "(%s%c%s)", op1, postfix[i], op2);
+    int i = 0;
+    while (postfix[i]  != '\0')
+    {
+        if (is_operator(postfix[i]))
+        {
+            char *op2 = pop(stack, &top);
+            char *op1 = pop(stack, &top);
+            char *temp = (char *)malloc(strlen(op1) + strlen(op2) + 4);
+            sprintf(temp, "(%s%c%s)", op1, postfix[i], op2);
             push(stack, &top, temp);
         }
+        else{
+            char * operand = (char *)malloc(2);
+            operand[0] = postfix[i];
+            operand[1] = '\0';
+            push(stack, &top, operand);
+        }
+        i++;
     }
-
-    pop(stack, &top, infix);  // The resulting infix expression
+    strcpy(infix,pop(stack,&top));
 }
+void main(){
+    char postfix[MAX_SIZE],infix[MAX_SIZE];
 
-int main() {
-    char postfix[MAX_SIZE], infix[MAX_SIZE];
-
-    printf("Enter the postfix expression: ");
+    printf("Enter the postfi expression:");
     scanf("%s", postfix);
 
-    postfix_to_infix(postfix, infix);
-
-    printf("Infix expression: %s\n", infix);
-
-    return 0;
+    postfix_to_infix(postfix,infix);
+    printf("infix expression:%s\n", infix);
+    getch();
 }
-
